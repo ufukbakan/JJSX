e8347f9825ec939e6d9c098a4b96efdc2b9106da
Ufuk Bakan <println.ufukbakan@gmail.com>
2025-03-12 16:24:42 +0300

    feat: compress dts

--- postbuild.mjs
+++ postbuild.mjs
@@ -6,13 +6,18 @@ import * as ts from "typescript";
 console.log("Minifying declaration files");
 const minifier = createMinifier(ts);
 glob("./dist/*.d.ts")
-  .then(files => {
-    files.forEach(file => {
-      const content = readFileSync(file, "utf-8");
-      const minified = minifier.minify(content);
-      writeFileSync(file, minified, { encoding: "utf-8" });
-      console.log("minified", file);
+  .then((files) => {
+    files.forEach((file) => {
+      try {
+        const content = readFileSync(file, "utf-8");
+        const minified = minifier.minify(content);
+        writeFileSync(file, minified, { encoding: "utf-8" });
+        console.log("Minified ✅", file);
+      } catch (e) {
+        console.error("Couldn't minify ❌", file);
+        console.error(e);
+      }
     });
     console.log("Completed minifying");
   })
-  .catch(e => console.error(e));
+  .catch((e) => console.error(e));
